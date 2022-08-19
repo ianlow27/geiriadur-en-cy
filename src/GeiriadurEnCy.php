3a12b3f4ebb6a74aedc7424bdff19b7fd1e1563b
Ian Low <ianlow28@gmail.com>
2022-08-19 16:35:40 +0100

    Ymrwymiad cychwyniedig

---
+++ src/GeiriadurEnCy.php
@@ -0,0 +1,32 @@
+<?php
+/* ==============================================
+TO DO:
+======
+- details
+-------------------------------------------------
+DONE:
+=====
+# Changelog
+## 1.0.0 - yyyy-mm-dd
+### Changed
+- details
+### Fixed
+- details
+### Added
+- details
+================================================= */
+namespace Ianl28\GeiriadurEnCy;
+class GeiriadurEnCy{
+
+  //-------------------------------
+  public function __construct(){
+    echo "This is called from GeiriadurEnCy->__construct()\n";
+  }//endfunc
+  //-------------------------------
+  public function testrun(){
+    echo "This is called from GeiriadurEnCy->testrun()\n";
+  }//endfunc
+  //-------------------------------
+
+}//endclass 
+?>
